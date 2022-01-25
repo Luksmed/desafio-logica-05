d3791313b8d10b18c4f392dee285e0fd460cef87
Luksmed <lukksme@gmail.com>
2022-01-24 21:28:08 -0300

    finalizado

---
+++ script.js
@@ -0,0 +1,14 @@
+let series = ["Dexter","Homeland","GOT","The Americans", "Westworld"]
+console.log(series)
+
+series.unshift("TWD")
+
+series.pop()
+
+series.push("Prison Break","Hannibal")
+
+series.shift()
+
+let numbers = [7,5,6,3,8,9,2,1,4]
+numbers.sort(function(a, b){return a - b})  
+console.log(numbers)
